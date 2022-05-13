152c2cb9d75b65c2e4444227f6e3f62820e862c4
princeew <ericprince82@gmail.com>
2022-05-13 13:06:15 -0600

    updating registerShiny

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,24 @@
 registerShiny <- function(shiny_directory) {
   shiny_directory <- normalizePath(shiny_directory)
   application_name <- rev(unlist(strsplit(shiny_directory, split = "/")))[1]
-  R.utils::copyDirectory(
-    from = shiny_directory,
-    to = paste0("/srv/shiny-server/", application_name)
-  )
+  target_directory <- paste0("/srv/shiny-server/", application_name)
+  
+  retries = 3
+  while (retries > 0) {
+    if (dir.exists(target_directory)) {
+      response <- readline(prompt = "Directory found matching this application. Overwrite? [Y/N] ")
+      if (response == "Y") {
+        R.utils::copyDirectory(
+          from = shiny_directory,
+          to = target_directory
+        )
+      } else if (response == "N") {
+        return()
+      } else {
+        retries <- retries - 1
+        logging("Response not understood. Please try again.", TRUE, "error")
+      }
+    }
+  }
+  
 }
